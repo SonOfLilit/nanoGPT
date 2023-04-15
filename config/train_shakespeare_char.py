f89066be3b15e8182a20074b38808fada5dddcc4
Aur Saraf <aur@sarafconsulting.com>
2023-04-15 19:40:21 +0000

    bigger training run

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -7,15 +7,15 @@ eval_iters = 200
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
 wandb_log = False # override via command line if you like
 wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt'
+wandb_run_name = 'hourglass-fast'
 
 dataset = 'shakespeare_char'
-batch_size = 8
-block_size = 256 # context of up to 256 previous characters
+batch_size = 6
+block_size = 4096 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 12
@@ -24,12 +24,12 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 1000
-lr_decay_iters = 1000 # make equal to max_iters usually
+max_iters = 24000
+lr_decay_iters = 24000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+beta2 = 0.96 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 100 # not supear necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
