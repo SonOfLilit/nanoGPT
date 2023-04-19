6321aeb066f2e2eebbab51abf250b28ac97e9af8
Aur Saraf <aur@sarafconsulting.com>
2023-04-19 14:46:49 +0300

    log loss, smooth loss

--- train.py
+++ train.py
@@ -246,6 +246,8 @@ X, Y = get_batch('train') # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
 raw_model = model.module if ddp else model # unwrap DDP container if needed
+last_losses = [0.0] * 16
+last_loss_index = 0
 running_mfu = -1.0
 while True:
 
@@ -316,10 +318,22 @@ while True:
         # get loss as float. note: this is a CPU-GPU sync point
         # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
         lossf = loss.item() * gradient_accumulation_steps
+        last_losses[last_loss_index % len(last_losses)] = lossf
+        last_loss_index += 1
+        if last_loss_index >= len(last_losses):
+            smooth_loss = sum(last_losses) / len(last_losses)
+        else:
+            smooth_loss = sum(last_losses[:last_loss_index]) / last_loss_index
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        print(f"iter {iter_num}: loss {lossf:.4f}, smooth: {smooth_loss:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        if wandb_log:
+            wandb.log({
+                "iter": iter_num,
+                "loss": lossf,
+                "smooth_loss": smooth_loss,
+            })
     iter_num += 1
     local_iter_num += 1
 
